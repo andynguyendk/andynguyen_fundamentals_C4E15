44ef723495823687a829c00997f697d7de63b367
Andy Nguyen <andynguyendk@gmail.com>
2018-01-14 14:41:18 +0700

    update

--- Homework2/polygons.py
+++ Homework2/polygons.py
@@ -2,11 +2,15 @@ import random
 from turtle import *
 shape("triangle")
 colors  = ["red","green","blue","orange","purple","pink","yellow"]
-speed(5)
-for i in range(3, 17):
+speed(0)
+for i in range(3, 10):
     color(random.choice(colors))
     for _ in range(i):
         forward(100)
         left(180-180*(i-2)/i)
+    seth(0)
+    for _ in range(i):
+        forward(100)
+        right(180-180*(i-2)/i)
     seth(0)
 mainloop()
