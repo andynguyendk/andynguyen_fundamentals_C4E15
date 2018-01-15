34e966e2f7d62a0a091745a3ba9ccc4777e760ed
Andy Nguyen <andynguyendk@gmail.com>
2018-01-15 18:10:33 +0700

    update

--- Homework2/print-inline.py
+++ Homework2/print-inline.py
@@ -1,5 +1,22 @@
-for i in range(10):
-    for i in range(10):
-        print("*", end ="")
-        print("x", end ="")
-    print(" ")
+#combined all a,b,c,d
+
+l = int(input("number of lines= "))
+b = int(input("number of blocks in a line= "))
+s = int(input("number of stars (in a block)= "))
+x = int(input("number of x(s) (in a block)= "))
+
+for i in range(l):
+    if i%2==1:
+        for i2 in range(b):
+            for i3 in range(s):
+                print("*", end ="")
+            for i4 in range(x):
+                print("x", end ="")
+        print(" ")
+    else:
+        for i2 in range(b):
+            for i3 in range(x):
+                print("x", end ="")
+            for i4 in range(s):
+                print("*", end ="")
+        print(" ")
